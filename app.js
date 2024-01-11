e248849cf4cbb5ac8fb54c82bc2a9ed0f267757e
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2024-01-11 14:49:12 +0530

    ADDED signup page and axios request

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const userModel = require('./FoodApp/models/userModel');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const cors= require('cors');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 const static_path = path.join(__dirname, "./FoodApp/public");
 app.use(express.static(static_path));
